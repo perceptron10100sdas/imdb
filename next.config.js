f7931cac4924b144b0db15bf711cef9145636756
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-06-29 16:18:00 +0530

    production change

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+
+
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      domains: ["image.tmdb.org"],
-    },
-  };
-  
-  module.exports = nextConfig;
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+};
+
+module.exports = nextConfig;
