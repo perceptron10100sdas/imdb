a2612e1db3903951c358fe886e6b647b7d6fc2e9
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2023-06-30 11:39:56 +0530

    trending and top rated changes

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default async function Home({ searchParams }) {
 
   const res = await fetch(
     `https://api.themoviedb.org/3/${
-      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+      genre === "fetchTopRated" ? "trending/all/week" : "movie/top_rated"
     }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
